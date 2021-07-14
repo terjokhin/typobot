302c0bf1bd2d8b8dde81ee68e4fccf6e8afb62d9
Aleksei Terekhin <daron666@yandex.ru>
2021-07-14 16:56:00 +0200

    added airbnb eslint rules

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,18 @@
-import { Telegraf } from "telegraf"
+import { Telegraf } from 'telegraf';
 
-
-const token: string = loadEnv("TOKEN")
-const bot = new Telegraf(token)
-
-bot.command('typobot', (ctx) => ctx.reply('new school'))
-bot.launch()
-
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
-
-function loadEnv(value: string): string {
-    const result = process.env[value]
-    if (typeof result === 'string') {
-        return result;
-    } else {
-        return '';
-    }
+function loadEnv(value: string) {
+  const result = process.env[value];
+  if (typeof result === 'string') {
+    return result;
+  }
+  throw new Error('Please provide token!');
 }
+
+const token: string = loadEnv('TOKEN');
+const bot = new Telegraf(token);
+
+bot.command('typobot', (ctx) => ctx.reply('new school'));
+bot.launch();
+
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
