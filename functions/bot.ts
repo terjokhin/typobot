512dbe3c52819b6c1dba4621fe412ad8662b8c31
Aleksei Terekhin <daron666@yandex.ru>
2021-08-18 17:15:14 +0200

    move function to it's own folder

---
+++ functions/bot.ts
@@ -0,0 +1,34 @@
+import { Scenes, session, Telegraf } from 'telegraf';
+import { Handler } from '@netlify/functions';
+import { getOrThrow } from '../src/utils/env';
+import antibotScene from '../src/scenes/Antibot';
+
+const token: string = getOrThrow('TOKEN2', () => new Error('Please provide token!'));
+const bot = new Telegraf<Scenes.SceneContext>(token);
+
+const stage = new Scenes.Stage([antibotScene]);
+
+bot.use(session());
+bot.use(stage.middleware());
+bot.command('antibot', (ctx) => ctx.scene.enter('antibot', { isBot: true }));
+
+bot.start((ctx) => {
+  ctx.reply('hey!');
+});
+
+const handler: Handler = async (event, _context) => {
+  const { body } = event;
+  if (typeof body === 'string') {
+    try {
+      await bot.handleUpdate(JSON.parse(body));
+      return { statusCode: 200, body: '' };
+    } catch (e) {
+      console.log(e);
+      return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
+    }
+  } else {
+    return { statusCode: 400, body: 'This endpoint is meant for bot and telegram communication' };
+  }
+};
+
+export { handler as default };
